825583f94795828e439772cf14e5faabfd3149aa
Sonneck <justin.sonneck@ambiomnode3.isas.de>
2022-12-08 14:03:08 +0100

    add cellpose

---
+++ 01_cellpose.py
@@ -0,0 +1,56 @@
+# !nvcc --version
+# !nvidia-smi
+
+import os
+import numpy as np
+from cellpose import core, models
+from glob import glob
+from aicsimageio import AICSImage
+from aicsimageio.writers import OmeTiffWriter
+
+
+use_GPU = core.use_gpu()
+yn = ['NO', 'YES']
+print(f'>>> GPU activated? {yn[use_GPU]}')
+
+
+dir = './example/raw/'
+movies = glob(dir + '*.tiff')
+
+for movie in movies:
+    well_name = os.path.basename(movie)[:-5]
+    savedir = './example/segmentation/'
+    model_path = './example/models/cellpose_residual_on_style_on_concatenation_off_01_2022_09_02_13_36_37.924639'
+    diameter=15
+    chan=0
+    chan2=0
+    flow_threshold = 0.4
+    cellprob_threshold=0
+
+
+    reader_image_stack = AICSImage(movie)
+    image_stack = reader_image_stack.get_image_data("ZYX")
+    images = []
+    files = []
+    for i in range(image_stack.shape[0]):
+        images.append(image_stack[i])
+        files.append("image_" + str(i) + ".tiff")
+    
+    # declare model
+    model = models.CellposeModel(gpu=True, 
+                                 pretrained_model=model_path)
+    
+    # use model diameter if user diameter is 0
+    diameter = model.diam_labels if diameter==0 else diameter
+    
+    # run model on test images
+    masks, flows, styles = model.eval(images, 
+                                      channels=[chan, chan2],
+                                      diameter=diameter,
+                                      flow_threshold=flow_threshold,
+                                      cellprob_threshold=cellprob_threshold
+                                      )
+
+    
+    masks_3d = np.vstack(np.expand_dims(masks, axis=0))
+    OmeTiffWriter.save(masks_3d, savedir + well_name + '.tiff', dim_order="ZYX")
